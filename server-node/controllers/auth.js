6e43c2bfef2ca1b9700b43698232e7e102a60c8c
Jordan Morris <jthecybertinkerer@gmail.com>
2020-06-30 06:03:36 -0500

    login and register implemented

--- server-node/controllers/auth.js
+++ server-node/controllers/auth.js
@@ -1,4 +1,61 @@
-function login(req, res) {}
-function register(req, res) {}
+const bcrypt = require("bcrypt");
+const massive = require("massive");
+function register(req, res, next) {
+  console.log("register called");
+  try {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) throw err;
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) throw err;
+        console.log("password hash", hash);
+        req.app
+          .get("db")
+          .user.register(req.body.username, hash, req.body.profile)
+          .then((result) => {
+            res.status(201).send("Registration Successful");
+          })
+          .catch((err) => {
+            if (err.code == 23505) {
+              console.error("user already exists, responding with 201");
+              res.status(201).send("Registration Successful");
+            } else {
+              console.error(err);
+              res.status(500).send();
+            }
+          });
+      });
+    });
+  } catch (err) {
+    console.error("an error occured!!!", err);
+    next(err);
+  }
+}
+function login(req, res, next) {
+  try {
+    req.app
+      .get("db")
+      .user.getByUsername(req.body.username)
+      .then((result) => {
+        let user = result[0] || {};
+        bcrypt.compare(
+          req.body.password,
+          user.password || "",
+          (err, result) => {
+            if (err || !result) {
+              res.status(401).send("Incorrect username or password");
+            } else {
+              res.json({
+                username: user.username,
+                profile: user.profile || "null",
+              });
+            }
+          }
+        );
+      });
+    // res.status(501).json(501);
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = { login, register };
--- server-node/routes.js
+++ server-node/routes.js
@@ -18,17 +18,51 @@ api.post(
   `${API_BASE_PATH}${AUTH_BASE_PATH}/login`,
   // requestGuards.expectJsonBody,
   function checkLoginBody(req, res, next) {
-    console.log(req.body);
-    next();
+    if (typeof req.body.username !== "string") {
+      res
+        .status(422)
+        .send(
+          "Missing or invalid type for username, recieved: " +
+            typeof req.body.username
+        );
+    } else if (req.body.username.length === 0) {
+      res.status(422).send("Username cannot be empty");
+    } else if (typeof req.body.password !== "string") {
+      res
+        .status(422)
+        .send(
+          "Missing or invalid type for password, recieved: " +
+            typeof req.body.password
+        );
+    } else if (req.body.password.length === 0) {
+      res.status(422).send("Password cannot be empty");
+    } else {
+      next();
+    }
   },
   auth.login
 );
 api.post(
   `${API_BASE_PATH}${AUTH_BASE_PATH}/register`,
   function checkRegistrationBody(req, res, next) {
-    auth.register;
+    if (
+      typeof req.body.username === "string" &&
+      typeof req.body.password === "string" &&
+      typeof req.body.profile === "string"
+    ) {
+      next();
+    } else {
+      res.status(422);
+      if (req.body.username == null) {
+        res.send("Missing username in request");
+      } else if (req.body.password == null) {
+        res.send("Missing username in request");
+      } else if (req.body.profile == null) {
+        res.send("Missing profile in request");
+      }
+    }
   },
-  notImplemented
+  auth.register
 );
 // posts
 api.post(`${API_BASE_PATH}${POSTS_BASE_PATH}/:userId(\\d+)`, posts.create);
